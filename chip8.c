7b48c335a1905dbbfbc56f1fb1ebbf204bdfea86
Artur Mroz <frost47@gmail.com>
2023-03-23 08:06:27 +0000

    draw window

--- chip8.c
+++ chip8.c
@@ -1,10 +1,111 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "SDL.h"
 
-int main(int argc, char **argv) {
-	(void)argc;
-	(void)argv;
+typedef struct {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+} sdl_t;
 
-	puts("hello");
+typedef struct {
+    uint16_t window_width;
+    uint16_t window_height;
+    uint32_t fg_color;
+    uint32_t bg_color;
+    uint16_t scale_factor;
+} config_t;
+
+bool init_sdl(sdl_t *sdl, const config_t config) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
+        SDL_Log("Failed to init SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    sdl->window = SDL_CreateWindow(
+        "Chip8 Emulator",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        config.window_width * config.scale_factor,
+        config.window_height * config.scale_factor,
+        SDL_WINDOW_OPENGL);
+
+    if (!sdl->window) {
+        SDL_Log("Failed to create a window: %s\n", SDL_GetError());
+        return false;
+    }
+
+    sdl->renderer = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
+    if (!sdl->renderer) {
+        SDL_Log("Failed to create a renderer: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+void final_cleanup(sdl_t sdl) {
+    SDL_DestroyRenderer(sdl.renderer);
+    SDL_DestroyWindow(sdl.window);
+    SDL_Quit();
+}
+
+bool set_config_from_args(config_t *config, const int argc, char **argv) {
+    // defaults
+    *config = (config_t){
+        .window_width  = 64,
+        .window_height = 32,
+        .fg_color      = 0x00AA00FF,
+        .bg_color      = 0xFF2222FF,
+        .scale_factor  = 10,
+    };
+
+    // overrides
+    for (int i = 1; i < argc; i++) {
+        (void)argv[i]; // shush unused vars warnings
+    }
+
+    return true;
+}
+
+void clear_screen(const sdl_t sdl, const config_t config) {
+    const uint8_t r = (config.bg_color >> 24) & 0xFF;
+    const uint8_t g = (config.bg_color >> 16) & 0xFF;
+    const uint8_t b = (config.bg_color >> 8) & 0xFF;
+    const uint8_t a = (config.bg_color >> 0) & 0xFF;
+
+    SDL_SetRenderDrawColor(sdl.renderer, r, g, b, a);
+    SDL_RenderClear(sdl.renderer);
+}
+
+void update_screen(const sdl_t sdl) {
+    SDL_RenderPresent(sdl.renderer);
+}
+
+int main(int argc, char **argv) {
+    // init config
+    config_t config = {0};
+    if (!set_config_from_args(&config, argc, argv)) exit(EXIT_FAILURE);
+
+    // init SDL
+    sdl_t sdl = {0};
+    if (!init_sdl(&sdl, config)) exit(EXIT_FAILURE);
+
+    clear_screen(sdl, config);
+
+    // main emulator loop
+    // for (;;)
+    {
+        // delay for ~60hz/60fps
+        SDL_Delay(16);
+
+        update_screen(sdl);
+        SDL_Delay(3000);
+    }
+
+    final_cleanup(sdl);
+
+    exit(EXIT_SUCCESS);
 }
